3abc34e94084412c11e3867c395cac018d2c5bb2
297179121 <297179121@qq.com>
2017-01-08 13:06:08 +0800

    demo例子

--- src/test/DemoTest.java
+++ src/test/DemoTest.java
@@ -5,21 +5,19 @@ import java.io.File;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class FileTest {
-	
+public class DemoTest {
+
 	@Test
 	public void testUnZip() {
 		try {
 			String pathSrcZip = "C:\\Users\\yhr\\Desktop\\tmp\\drs.zip";
 			String pathDesZip = "D:\\mywork\\project\\eclipsespace\\hscr\\WebContent\\doc\\drs\\21130219871126085X\\2017-01-08";
 			File fileSrcZip = new File(pathSrcZip);
-			//File fileDesZip = new File(pathDesZip, "drs.zip");
-			YFile.decompress( fileSrcZip, pathDesZip );
+			YFile.unZip( fileSrcZip, pathDesZip );
 		} catch ( Exception e) {
 			e.printStackTrace();
 			Assert.fail(e.getMessage());
 		}
 	}
-
 
 }
