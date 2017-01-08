2104bfa6e2945107263caa7dcb47326577aa61aa
297179121 <297179121@qq.com>
2017-01-08 13:32:02 +0800

    编码

--- src/test/YEncode.java
+++ src/test/YEncode.java
@@ -8,8 +8,18 @@ import java.io.IOException;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.io.IOUtils;
 
+/**
+ * 将文件、字符串加密成指定编码
+ * @author yhr
+ *
+ */
 public class YEncode {
 	
+	/**
+	 * 将文件按base64位编码
+	 * @param file
+	 * @return
+	 */
 	public static String toBase64( File file ){
 		byte[] array = new byte[]{};
 		try {
